344db6282208b2b3616827f1ff35a794a6973d89
Thiago Dias <thdteixeira2@gmail.com>
2024-05-26 17:55:55 -0300

    first commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <img src="" alt="Logo" />
+        <div>
+          <ul>
+            <li>
+              <a href="/">Sobre nós</a>
+            </li>
+            <li>
+              <a href="/">Nossos Clientes</a>
+            </li>
+          </ul>
+          <button>Entre em contato</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      // {
+      //   path: "/about-us",
+      //   element: <AboutUs/>
+      // }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
